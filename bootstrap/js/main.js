e176373c6ef407403f3dec130e0508ff85ce164e
egin10 <egin066@gmail.com>
2017-07-03 22:31:14 +0800

    add validation

--- bootstrap/js/main.js
+++ bootstrap/js/main.js
@@ -7,6 +7,10 @@ function saveBookmark(e){
   var siteName = document.getElementById('siteName').value;
   var siteUrl = document.getElementById('siteUrl').value;
 
+  if(!validateForm(siteName, siteUrl)){
+    return false;
+  }
+
   var bookmark = {
     name: siteName,
     url: siteUrl
@@ -37,9 +41,34 @@ function saveBookmark(e){
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
 
+  //Re-fetch Bookmark
+  fetchBookmarks();
+
+  //Clear form
+  document.getElementById('myForm').reset();
+
   //Prevent Form From Submitting
   e.preventDefault();
 }
+
+//Delete Bookmark
+function deleteBookmark(url){
+  //Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  //Loop throught bookmarks
+  for(var i = 0; i < bookmarks.length; i++){
+    if(bookmarks[i].url == url){
+      //Remove from array
+      bookmarks.splice(i, 1);
+    }
+  }
+  //Re-set back to localStorage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  //Re-fetch Bookmark
+  fetchBookmarks();
+}
+
 
 //Fetch bookmarks
 function fetchBookmarks(){
@@ -49,6 +78,7 @@ function fetchBookmarks(){
   var bookmarksResults = document.getElementById('bookmarksResults');
   //Build Output
   bookmarksResults.innerHTML = '';
+  
   for(var i = 0; i < bookmarks.length; i++){
     var name = bookmarks[i].name;
     var url = bookmarks[i].url;
@@ -56,7 +86,29 @@ function fetchBookmarks(){
     bookmarksResults.innerHTML += '<div class="well">'+
                                   '<h3>' + name +
                                   ' <a class="btn btn-default" target="_blank" href="'+ url +'">Visit</a>'+
+                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>'+
                                   '<h3>'+
                                   '</div>';
   }
+}
+
+//Validate Form Input
+function validateForm(siteName, siteUrl){
+  //Validation parsing data
+  if(!siteName || !siteUrl){
+    alert('Please fill Site Name and Site Url in the form');
+    return false;
+  }
+
+  //Regular Expression to match an email
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  //Check if siteUrl not matching an email
+  if(!siteUrl.match(regex)){
+    alert('Please use a valid URL');
+    return false;
+  }
+
+  return true;
 }
